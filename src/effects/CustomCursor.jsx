b7f6dbdb00bbbcef988d82a308465ea4c9bae2bd
Abdou <fallpapaabdourahmane@gmail.com>
2026-06-05 22:00:13 +0000

    style: suppression du point blanc du curseur, restauration du pointeur classique tout en gardant l'effet de halo néon

--- src/effects/CustomCursor.jsx
+++ src/effects/CustomCursor.jsx
@@ -32,11 +32,6 @@ const CustomCursor = () => {
         className="pointer-events-none fixed top-0 left-0 z-[9998] w-24 h-24 rounded-full bg-[var(--color-neon-blue)] opacity-20 blur-xl mix-blend-screen transition-transform duration-300 ease-out"
         style={{ transform: `translate(${position.x - 48}px, ${position.y - 48}px)` }}
       />
-      {/* The actual dot */}
-      <div 
-        className="pointer-events-none fixed top-0 left-0 z-[9999] w-3 h-3 rounded-full bg-white shadow-[0_0_10px_var(--color-neon-blue)] transition-transform duration-75 ease-out"
-        style={{ transform: `translate(${position.x - 6}px, ${position.y - 6}px)` }}
-      />
     </>
   );
 };
